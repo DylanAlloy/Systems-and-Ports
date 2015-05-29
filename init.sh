9f688c35d2669a111c5dbb4bea4e9f83a24b4aa7
Dylan Moore <hosnappp@gmail.com>
2015-05-29 14:44:26 +0000

    Create init.sh
    initialization script for the port scanner. detects installed software and proceeds with user prompt.

---
+++ init.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+#INTRODUCTION
+echo ------------
+tput setaf 3
+echo Systems and Ports Copyright (C) 2013 Dylan Moore
+tput sgr0
+echo This program comes with ABSOLUTELY NO WARRANTY; for details see the git.
+echo This is free software, and you are welcome to redistribute it
+echo under certain conditions, see the git for more info.
+echo ------------
+
+#DISTRO DETECT
+	if [ -f /etc/debian_version ]; then
+		PKG1=$(dpkg-query -W --showformat='${Status}\n' libnotify-bin|grep "install ok installed")
+		if [ "" == "$PKG1" ]; then
+			sudo apt-get --force-yes --yes install libnotify-bin
+			notify-send "Systems & Ports Notifications (libnotify) installed."
+		fi
+wait
+		PKG2=$(dpkg-query -W --showformat='${Status}\n' nmap|grep "install ok installed")
+			echo Checking for nmap installation...
+		if [ "" == "$PKG2" ]; then
+			echo "No nmap installation detected. Setting up nmap..."
+  			sudo apt-get --force-yes --yes install nmap
+  			notify-send "Network Mapper (nmap) installed."
+		fi
+	
+
+	else
+		PKG1=$(dpkg-query -W --showformat='${Status}\n' libnotify-bin|grep "install ok installed")
+		if [ "" == "$PKG1" ]; then
+			sudo yum --force-yes --yes install libnotify-bin
+			notify-send "Systems & Ports Notifications (libnotify) installed."
+		fi
+wait
+		PKG2=$(dpkg-query -W --showformat='${Status}\n' nmap|grep "install ok installed")
+			echo Checking for nmap installation...
+		if [ "" == "$PKG2" ]; then
+  			echo "No nmap installation detected. Setting up nmap..."
+  			sudo yum --force-yes --yes install nmap
+  			notify-send "Network Mapper (nmap) installed."
+		fi
+	
+	fi
+	
+
+sleep 3
+clear
+
+# TIME TO ASK WHAT THEY WANT
+. menu.sh
+
